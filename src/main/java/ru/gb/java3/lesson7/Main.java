32723e88f7528513016b8f94df651325a3dc1a4e
AntonKurbet <anton.kurbet@gmail.com>
2020-11-25 01:52:25 +0300

    + spiral matrix

--- src/main/java/ru/gb/java3/lesson7/Main.java
+++ src/main/java/ru/gb/java3/lesson7/Main.java
@@ -58,6 +58,73 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+// 3. Заполнить матрицу по спирали
+//        2 варианта 1й закомментирован
+        int[][] matrix = new int[5][7];
+        fillSpiral(matrix);
+        printMatrix(matrix);
+    }
+
+    private static void printMatrix(int[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                System.out.printf("%3d", m[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fillSpiral(int[][] m) {
+        // int c = 1, max = m.length * m[0].length; // VAR1
+        int c = 0, max = m.length * m[0].length;
+        for (int q = 0; q < Math.max(m.length / 2, m[0].length / 2); q++) {
+        //     c = fillSquare(m, c, max, q,q,m.length - q - 1, m[0].length - q - 1); // VAR1
+            c = fillSquare2(m, c, max, q,q,m.length - q - 1, m[0].length - q - 1);
+        }
+    }
+
+//    VAR1
+//    private static int fillSquare(int[][] m, int c, int max, int x1, int y1, int x2, int y2) {
+//        for (int i = y1; i <=y2; i++) {
+//            m[x1][i] = c++;
+//        }
+//        if (c > max) return max;
+//        for (int j = x1 + 1; j <= x2; j++) {
+//            m[j][y2] = c++;
+//        }
+//        if (c > max) return max;
+//        for (int i = y2 - 1; i >= y1; i--) {
+//            m[x2][i] = c++;
+//        }
+//        if (c > max) return max;
+//        for (int j = x2 - 1; j > x1; j--) {
+//            m[j][y1] = c++;
+//        }
+//        return c;
+//    }
+
+
+    private static int fillSquare2(int[][] m, int c, int max, int x1, int y1, int x2, int y2) {
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+        int r = dx + dy;
+
+        for (int y = 0; y < dy; y++) {
+            c++;
+            m[x1][y1 + y] = c;
+            if (c + r > max) return max;
+            m[x2][y2 - y] = c + r;
+        }
+        for (int x = 0; x < dx; x++) {
+            c++;
+            m[x1 + x][y2] = c;
+            if (c + r > max) return max;
+            m[x2 - x][y1] = c + r;
+        }
+
+        c += r;
+        return c;
     }
 
     private static void CheckClass(URI path) throws MalformedURLException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
@@ -73,4 +140,5 @@ public class Main {
                 }
         System.out.println("Test failed");
     }
+
 }
